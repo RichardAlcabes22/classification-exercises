a97d3a807b35da9b9836b2e578a1e805fc306b42
Rich Alcabes <alcabes.richard@gmail.com>
2023-03-14 17:00:40 -0500

    added modeling

--- acquire.py
+++ acquire.py
@@ -82,17 +82,22 @@ def new_telco_data():
     '''
     This function reads the telco data from the Codeup db into a df.
     '''
-    sql_query = """
+    filename = "telco_new.csv"
+
+    if os.path.isfile(filename):
+        return pd.read_csv(filename,index_col=False)
+    else:
+        sql_query = """
                 select * from customers
                 join contract_types using (contract_type_id)
                 join internet_service_types using (internet_service_type_id)
                 join payment_types using (payment_type_id)
                 """
     
-    # Read in DataFrame from Codeup db.
-    df = pd.read_sql(sql_query, get_sql_url('telco_churn'))
-    
-    return df
+        # Read in DataFrame from Codeup db.
+        df = pd.read_sql(sql_query, get_sql_url('telco_churn'))
+        df.to_csv(filename,index=False)
+        return df
 
 
 #----------------------------------------------------
